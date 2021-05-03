0498c482a6902394cd9b593deab1cd9f1e5e4198
Dallas-Marshall <dallas.marshall@my.jcu.edu.au>
2021-05-03 12:40:32 +1000

    Update default JavaScript to work for dropdowns

--- functions.php
+++ functions.php
@@ -7,12 +7,12 @@
  * @package JazzClubTownsville
  */
 
-if ( ! defined( '_S_VERSION' ) ) {
+if (!defined('_S_VERSION')) {
 	// Replace the version number of the theme on each release.
-	define( '_S_VERSION', '1.0.0' );
+	define('_S_VERSION', '1.0.0');
 }
 
-if ( ! function_exists( 'jazzclubtownsville_setup' ) ) :
+if (!function_exists('jazzclubtownsville_setup')) :
 	/**
 	 * Sets up theme defaults and registers support for various WordPress features.
 	 *
@@ -20,17 +20,18 @@ if ( ! function_exists( 'jazzclubtownsville_setup' ) ) :
 	 * runs before the init hook. The init hook is too late for some features, such
 	 * as indicating support for post thumbnails.
 	 */
-	function jazzclubtownsville_setup() {
+	function jazzclubtownsville_setup()
+	{
 		/*
 		 * Make theme available for translation.
 		 * Translations can be filed in the /languages/ directory.
 		 * If you're building a theme based on JazzClubTownsville, use a find and replace
 		 * to change 'jazzclubtownsville' to the name of your theme in all the template files.
 		 */
-		load_theme_textdomain( 'jazzclubtownsville', get_template_directory() . '/languages' );
+		load_theme_textdomain('jazzclubtownsville', get_template_directory() . '/languages');
 
 		// Add default posts and comments RSS feed links to head.
-		add_theme_support( 'automatic-feed-links' );
+		add_theme_support('automatic-feed-links');
 
 		/*
 		 * Let WordPress manage the document title.
@@ -38,19 +39,19 @@ if ( ! function_exists( 'jazzclubtownsville_setup' ) ) :
 		 * hard-coded <title> tag in the document head, and expect WordPress to
 		 * provide it for us.
 		 */
-		add_theme_support( 'title-tag' );
+		add_theme_support('title-tag');
 
 		/*
 		 * Enable support for Post Thumbnails on posts and pages.
 		 *
 		 * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
 		 */
-		add_theme_support( 'post-thumbnails' );
+		add_theme_support('post-thumbnails');
 
 		// This theme uses wp_nav_menu() in one location.
 		register_nav_menus(
 			array(
-				'menu-1' => esc_html__( 'Primary', 'jazzclubtownsville' ),
+				'menu-1' => esc_html__('Primary', 'jazzclubtownsville'),
 			)
 		);
 
@@ -84,7 +85,7 @@ if ( ! function_exists( 'jazzclubtownsville_setup' ) ) :
 		);
 
 		// Add theme support for selective refresh for widgets.
-		add_theme_support( 'customize-selective-refresh-widgets' );
+		add_theme_support('customize-selective-refresh-widgets');
 
 		/**
 		 * Add support for core custom logo.
@@ -94,15 +95,15 @@ if ( ! function_exists( 'jazzclubtownsville_setup' ) ) :
 		add_theme_support(
 			'custom-logo',
 			array(
-				'height'      => 1000,
-				'width'       => 250,
-				'flex-width'  => true,
+				'height' => 1000,
+				'width' => 250,
+				'flex-width' => true,
 				'flex-height' => true,
 			)
 		);
 	}
 endif;
-add_action( 'after_setup_theme', 'jazzclubtownsville_setup' );
+add_action('after_setup_theme', 'jazzclubtownsville_setup');
 
 /**
  * Register custom fonts.
@@ -151,48 +152,59 @@ function jazzclubtownsville_fonts_url()
  *
  * @global int $content_width
  */
-function jazzclubtownsville_content_width() {
-	$GLOBALS['content_width'] = apply_filters( 'jazzclubtownsville_content_width', 640 );
+function jazzclubtownsville_content_width()
+{
+	$GLOBALS['content_width'] = apply_filters('jazzclubtownsville_content_width', 640);
 }
-add_action( 'after_setup_theme', 'jazzclubtownsville_content_width', 0 );
+
+add_action('after_setup_theme', 'jazzclubtownsville_content_width', 0);
 
 /**
  * Register widget area.
  *
  * @link https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
  */
-function jazzclubtownsville_widgets_init() {
+function jazzclubtownsville_widgets_init()
+{
 	register_sidebar(
 		array(
-			'name'          => esc_html__( 'Sidebar', 'jazzclubtownsville' ),
-			'id'            => 'sidebar-1',
-			'description'   => esc_html__( 'Add widgets here.', 'jazzclubtownsville' ),
+			'name' => esc_html__('Sidebar', 'jazzclubtownsville'),
+			'id' => 'sidebar-1',
+			'description' => esc_html__('Add widgets here.', 'jazzclubtownsville'),
 			'before_widget' => '<section id="%1$s" class="widget %2$s">',
-			'after_widget'  => '</section>',
-			'before_title'  => '<h2 class="widget-title">',
-			'after_title'   => '</h2>',
+			'after_widget' => '</section>',
+			'before_title' => '<h2 class="widget-title">',
+			'after_title' => '</h2>',
 		)
 	);
 }
-add_action( 'widgets_init', 'jazzclubtownsville_widgets_init' );
+
+add_action('widgets_init', 'jazzclubtownsville_widgets_init');
 
 /**
  * Enqueue scripts and styles.
  */
-function jazzclubtownsville_scripts() {
+function jazzclubtownsville_scripts()
+{
 	// Enqueue Google Fonts: Source Sans Pro and Source Serif Pro
 	wp_enqueue_style('jazzclubtownsville-fonts', jazzclubtownsville_fonts_url());
 
-	wp_enqueue_style( 'jazzclubtownsville-style', get_stylesheet_uri(), array(), wp_get_theme()->get('Version') );
-	wp_style_add_data( 'jazzclubtownsville-style', 'rtl', 'replace' );
+	wp_enqueue_style('jazzclubtownsville-style', get_stylesheet_uri(), array(), wp_get_theme()->get('Version'));
+	wp_style_add_data('jazzclubtownsville-style', 'rtl', 'replace');
 
-	wp_enqueue_script( 'jazzclubtownsville-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true );
+	wp_enqueue_script('jazzclubtownsville-navigation', get_template_directory_uri() . '/js/navigation.js', array('jquery'), _S_VERSION, true);
 
-	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
-		wp_enqueue_script( 'comment-reply' );
+	wp_localize_script('jazzclubtownsville-navigation', 'jazzclubtownsvilleScreenReaderText', array(
+		'expand' => __('Expand child menu', 'jazzclubtownsville'),
+		'collapse' => __('Collapse child menu', 'jazzclubtownsville')
+	));
+
+	if (is_singular() && comments_open() && get_option('thread_comments')) {
+		wp_enqueue_script('comment-reply');
 	}
 }
-add_action( 'wp_enqueue_scripts', 'jazzclubtownsville_scripts' );
+
+add_action('wp_enqueue_scripts', 'jazzclubtownsville_scripts');
 
 /**
  * Implement the Custom Header feature.
@@ -217,7 +229,7 @@ require get_template_directory() . '/inc/customizer.php';
 /**
  * Load Jetpack compatibility file.
  */
-if ( defined( 'JETPACK__VERSION' ) ) {
+if (defined('JETPACK__VERSION')) {
 	require get_template_directory() . '/inc/jetpack.php';
 }
 
