143b3fda3c90c57758157d69293c8879f2f9b6be
HarperT <harper.thurlow@my.jcu.edu.au>
2021-05-04 17:11:23 +1000

    fix Social Menu overlapping with other elements of footer

--- footer.php
+++ footer.php
@@ -11,24 +11,24 @@
 
 ?>
 
-	<footer id="colophon" class="site-footer">
-		<div class="back-to-top">
-			<a href="">Back to top</a>
+<footer id="colophon" class="site-footer">
+	<div class="back-to-top">
+		<a href="">Back to top</a>
 
-		</div> <!-- .back-to-top -->
-		<nav class="social-nav">
-			<!--WP nav menu linked to social menu. Seen in Customize -> Menus -> Social Media Menu-->
-			<?php wp_nav_menu( array ('theme_location' => 'social')); ?>
-		</nav> <!-- .social-nav -->
-		<div class="site-info">
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s %2$s.', 'jazzclubtownsville' ), '&copy;', '<a href="https://github.com/cp3402-students/cp3402-2021-site-cp3402-2021-team04">Team 04</a>' );
-				?>
-		</div><!-- .site-info -->
+	</div> <!-- .back-to-top -->
+	<nav class="social-nav">
+		<!--WP nav menu linked to social menu. Seen in Customize -> Menus -> Social Media Menu-->
+		<?php wp_nav_menu(array('theme_location' => 'social')); ?>
+	</nav> <!-- .social-nav -->
+	<div class="site-info">
+		<?php
+
+		printf(esc_html__('Theme: %1$s %2$s.', 'jazzclubtownsville'), '&copy;', '<a href="https://github.com/cp3402-students/cp3402-2021-site-cp3402-2021-team04">Team 04</a>');
+		?>
+	</div><!-- .site-info -->
 
 
-	</footer><!-- #colophon -->
+</footer><!-- #colophon -->
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
