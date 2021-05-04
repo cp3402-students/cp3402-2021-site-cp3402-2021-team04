362c00be4b9c086cf21aaa4167ef4a3333ea11f6
HarperT <harper.thurlow@my.jcu.edu.au>
2021-05-04 18:38:04 +1000

    change social menu to footer menu for consistency

--- functions.php
+++ functions.php
@@ -51,8 +51,7 @@ if ( ! function_exists( 'jazzclubtownsville_setup' ) ) :
 		register_nav_menus(
 			array(
 				'menu-1' => esc_html__( 'Primary', 'jazzclubtownsville' ),
-				'social' => esc_html__( 'Social Media Menu', 'jazzclubtownsville' ),
-
+				'footer-menu' => esc_html__( 'Footer Menu', 'jazzclubtownsville' ),
 
 			)
 
