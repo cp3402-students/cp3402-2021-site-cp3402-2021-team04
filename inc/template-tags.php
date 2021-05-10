4f3da30b05fcd5388db9756debd5eae4d52075e9
Ryan <rquinlan2@gmail.com>
2021-05-10 21:11:12 +1000

    Moved Location of Post Categories above Post.

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -57,14 +57,8 @@ if ( ! function_exists( 'jazzclubtownsville_entry_footer' ) ) :
 	 * Prints HTML with meta information for the categories, tags and comments.
 	 */
 	function jazzclubtownsville_entry_footer() {
-		// Hide category and tag text for pages.
+		// Hide tag text for pages.
 		if ( 'post' === get_post_type() ) {
-			/* translators: used between list items, there is a space after the comma */
-			$categories_list = get_the_category_list( esc_html__( ', ', 'jazzclubtownsville' ) );
-			if ( $categories_list ) {
-				/* translators: 1: list of categories. */
-				printf( '<span class="cat-links">' . esc_html__( 'Posted in %1$s', 'jazzclubtownsville' ) . '</span>', $categories_list ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
-			}
 
 			/* translators: used between list items, there is a space after the comma */
 			$tags_list = get_the_tag_list( '', esc_html_x( ', ', 'list item separator', 'jazzclubtownsville' ) );
@@ -111,7 +105,14 @@ if ( ! function_exists( 'jazzclubtownsville_entry_footer' ) ) :
 		);
 	}
 endif;
-
+function jazzclubtownsville_the_category_list() {
+	/* translators: used between list items, there is a space after the comma */
+	$categories_list = get_the_category_list( esc_html__( ', ', 'jazzclubtownsville' ) );
+	if ( $categories_list ) {
+		/* translators: 1: list of categories. */
+		printf( '<span class="cat-links">' . esc_html__( '%1$s', 'jazzclubtownsville' ) . '</span>', $categories_list ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+	}
+}
 if ( ! function_exists( 'jazzclubtownsville_post_thumbnail' ) ) :
 	/**
 	 * Displays an optional post thumbnail.
