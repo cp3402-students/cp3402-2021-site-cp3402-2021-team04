dcbe5a2db8a0f0e06f267125e4e2d8150c3a0174
Dallas-Marshall <dallas.marshall@my.jcu.edu.au>
2021-05-13 09:31:14 +1000

    Add custom theme 404 page layout

--- 404.php
+++ 404.php
@@ -7,54 +7,26 @@
  * @package JazzClubTownsville
  */
 
-get_header();
-?>
+get_header(); ?>
 
-	<main id="primary" class="site-main">
+<div class="content-area">
+	<div class="site-content not-found-content">
 
-		<section class="error-404 not-found">
-			<header class="page-header">
-				<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'jazzclubtownsville' ); ?></h1>
-			</header><!-- .page-header -->
+		<header class="page-header">
+			<h1 class="page-title">Page Not Found</h1>
+		</header>
 
+		<div class="page-wrapper">
 			<div class="page-content">
-				<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'jazzclubtownsville' ); ?></p>
-
-					<?php
-					get_search_form();
-
-					the_widget( 'WP_Widget_Recent_Posts' );
-					?>
-
-					<div class="widget widget_categories">
-						<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'jazzclubtownsville' ); ?></h2>
-						<ul>
-							<?php
-							wp_list_categories(
-								array(
-									'orderby'    => 'count',
-									'order'      => 'DESC',
-									'show_count' => 1,
-									'title_li'   => '',
-									'number'     => 10,
-								)
-							);
-							?>
-						</ul>
-					</div><!-- .widget -->
-
-					<?php
-					/* translators: %1$s: smiley */
-					$jazzclubtownsville_archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'jazzclubtownsville' ), convert_smilies( ':)' ) ) . '</p>';
-					the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$jazzclubtownsville_archive_content" );
-
-					the_widget( 'WP_Widget_Tag_Cloud' );
-					?>
-
-			</div><!-- .page-content -->
-		</section><!-- .error-404 -->
-
-	</main><!-- #main -->
-
-<?php
-get_footer();
+				<h2>Thank you for checking out our site!</h2>
+				<p>It looks you have ventured too deep, if you believe this to be an error please contact the site
+					administration.</p>
+				<p>Visit our <a class="back-to-home" href="http://jazz.dallas-marshall.com">Home Page</a> to keep
+					browsing. We'll do better next time.</p>
+			</div>
+		</div>
+	</div>
+</div>
+<div class="404">
+	<?php get_footer(); ?>
+</div>
