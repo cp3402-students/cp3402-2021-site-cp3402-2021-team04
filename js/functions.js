f5299ad9d5360d9c09d7d0435fe785bbd156fdc1
Ryan <rquinlan2@gmail.com>
2021-05-12 13:42:32 +1000

    Add function.js with function to align-center image to make it full-bleed. Add enqueue of functions.js. to functions.php.

--- js/functions.js
+++ js/functions.js
@@ -2,3 +2,9 @@ function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
+
+(function($){
+  $('figure.wp-caption.aligncenter').removeAttr('style');
+  $('img.aligncenter').wrap('<figure class="centered-image" />');
+})(jQuery);
+
